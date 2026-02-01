b1b5f802b7ad743b4ed2bd44abe19b5d7f034d2f
aack37 <alexackler@gmail.com>
2026-02-01 16:18:26 -0500

    Final commits
    Surely

--- Assets/Script/GameManagerSc.cs
+++ Assets/Script/GameManagerSc.cs
@@ -6,15 +6,33 @@ public class GameManagerSc : MonoBehaviour
 {
     public TextAsset textFile;
 
+    public GameObject TitleCanvas;
+    public GameObject[] OtherCanvases;
+    public GameObject explanation;
+    public GameObject FullCanvas;
+
+    public void begin()
+    {
+        explanation.SetActive(true);
+        TitleCanvas.SetActive(false);
+    }
+
+    public void begin2()
+    {
+        explanation.SetActive(false);
+        foreach (GameObject g in OtherCanvases)
+        {
+            g.SetActive(true);
+        }
+        FullCanvas.SetActive(false);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        foreach (GameObject g in OtherCanvases)
+        {
+            g.SetActive(false);
+        }
     }
 }
